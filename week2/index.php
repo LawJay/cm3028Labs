106284e008b642bf8a59212d75abbf609da1c8bb
1608354 <a>
2016-10-03 15:56:07 +0100

    Initial setup - week 2

--- week2/index.php
+++ week2/index.php
@@ -14,3 +14,31 @@ echo "My name is" . $myname . "and i am " . $myage;
 
 echo "<html>One line \r\n Second line</html>";
 
+$name = "Jamie";
+print $name;
+
+if ($name == "John"){
+    print "I know you";
+
+}
+else {
+    print "Who are you?";
+}
+
+$numberofhobbits = 2;
+
+switch ($numberofhobbits){
+    case 1:
+        echo "1 sad hobbit :(";
+        break;
+    case 2:
+        echo "2 happy hobbits :)";
+        break;
+    case 3:
+        echo "3 hobbits are a crowd :D";
+        break;
+    default:
+        echo "All the hobbits have gone home";
+
+}
+

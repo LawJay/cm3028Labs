0d1241f0adf442761c595f3e44d4d7eb74ed5a77
1608354 <a>
2016-10-17 15:41:12 +0100

    get check

---
+++ Week4/shieldresponse.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1608354
+ * Date: 17/10/2016
+ * Time: 15:36
+ */
+
+
+$name = $_POST["name"];
+$gender = $_POST["gender"];
+$power = $_POST["power"];
+
+echo "Hello {$name}|{$gender}|{$power}";
+
+
+?>

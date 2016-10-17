30a63c558ad9271fdcfdb171be0e0af6c153cd89
1608354 <a>
2016-10-17 16:46:56 +0100

    adminer

---
+++ Week4/DBsuperheroes.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1608354
+ * Date: 17/10/2016
+ * Time: 16:37
+ */
+
+include(DBconnect.php);
+
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$superpower = $_POST["power"];
+
+$sql = "INSERT INTO superheroes (firstname,lastname,power) VALUES ('$firstname','$lastname','$superpower')";
--- Week4/shieldresponse.php
+++ Week4/shieldresponse.php
@@ -6,12 +6,19 @@
  * Time: 15:36
  */
 
+include ("DBconnect.php");
+
+
 
 $name = $_POST["name"];
 $gender = $_POST["gender"];
 $power = $_POST["power"];
 
+$sql = "INSERT INTO superheroes"(name,)
+
 echo "Hello {$name}|{$gender}|{$power}";
+
+
 
 
 ?>

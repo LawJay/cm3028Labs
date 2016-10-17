7d0b5ce89f2b596e133bf487e9969420827c23a5
1608354 <a>
2016-10-17 14:09:50 +0100

    select all

--- Week4/helloselector.php
+++ Week4/helloselector.php
@@ -6,6 +6,8 @@
 <h1> Where would you like to say hello to?</h1>
 
 <a href="helloprinter.php?planet=earth">Earth</a>
+<a href="helloprinter.php?planet=Mars">Mars</a>
+<a href="helloprinter.php?planet=Jupiter">Jupiter</a>
 
 </body>
 

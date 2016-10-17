7693a906b7b18808bc199136bbf977b4e44fd278
1608354 <a>
2016-10-17 14:51:59 +0100

    get check

--- Week4/formResponse.php
+++ Week4/formResponse.php
@@ -1,14 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 1608354
- * Date: 17/10/2016
- * Time: 14:18
- */
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
-$forename = $_POST["forename"];
-$surname = $_POST["surname"];
-echo $forename . $surname;
+$forename = $_POST['forename'];
+$surname = $_POST['surname'];
+
+echo "Hello {$forename}{$surname}";
+
+?>

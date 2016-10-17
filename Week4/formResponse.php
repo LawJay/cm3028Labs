be739de0a91d2b1c89d98cdf118c740834043562
1608354 <a>
2016-10-17 14:48:46 +0100

    get check

--- Week4/formResponse.php
+++ Week4/formResponse.php
@@ -5,7 +5,9 @@
  * Date: 17/10/2016
  * Time: 14:18
  */
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $forename = $_POST["forename"];
 $surname = $_POST["surname"];

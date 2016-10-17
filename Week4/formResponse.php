59d0eae3a35e488ed9414344cba7d8868bf407a6
1608354 <a>
2016-10-17 14:22:51 +0100

    select all

---
+++ Week4/formResponse.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1608354
+ * Date: 17/10/2016
+ * Time: 14:18
+ */
+
+
+$forename = $_POST["forename"];
+$surname = $_POST["surname"];
+echo $forename . $surname;

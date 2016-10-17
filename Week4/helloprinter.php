e5047731af61fb4e8aca185c32a3b511ba027991
1608354 <a>
2016-10-17 14:01:31 +0100

    select all

---
+++ Week4/helloprinter.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1608354
+ * Date: 17/10/2016
+ * Time: 13:50
+ */
---
+++ Week4/helloselector.php
@@ -0,0 +1,24 @@
+
+
+
+<html>
+<body>
+<h1> Where would you like to say hello to?</h1>
+
+<a href="1608354.azurewebsites.net/helloselector.php">Earth</a>
+
+</body>
+
+
+</html>
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1608354
+ * Date: 17/10/2016
+ * Time: 13:50
+ */
+
+

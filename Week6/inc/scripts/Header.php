2e396ba178c440a2212421697e89f40a85e4bfba
1608354 <a>
2016-10-31 15:49:15 +0000

    Week 6

--- Week6/inc/scripts/Header.php
+++ Week6/inc/scripts/Header.php
@@ -1,17 +1,22 @@
+<?
 session_start();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>The Blog of Charles Babbage</title>
+    <link rel="stylesheet" href="style/style.css">
+
 </head>
+
 <body>
 <header>
     <h1>The Blog of Charles Babbage</h1>
     <nav>
         <ul>
-            <li><a href="../../">Home Page</a></li>
+            <li><a href="./">Home Pagezzz</a></li>
             <li><a href="blog">My Blog</a></li>
             <li><a href="about">About Me</a></li>
             <li><a href="contactus">Contact Me</a></li>
@@ -23,6 +28,7 @@ session_start();
                 echo "<li><a href='login'>Login</a></li>";
             }
             ?>
+
         </ul>
     </nav>
 </header>

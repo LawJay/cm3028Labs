051fb029c1252d50921c6aab2dec63d4d84a9d76
Jordan <jwyoungman96@hotmail.co.uk>
2016-11-09 19:18:16 +0000

    Jordan

--- GraniteCityGeeks.github.io/ClubsWebsite/Clubs.php
+++ GraniteCityGeeks.github.io/ClubsWebsite/Clubs.php
@@ -1,11 +1,34 @@
-<?php
-    include("Header&Footer.header.php");
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="CoreCSS/style.css">
+    <link rel="stylesheet" href="CoreCSS/unsemantic-grid-responsive-tablet.css">
+</head>
+<body>
+<header>
+    <h1>The Blog of Charles Babbage</h1>
+    <nav>
+        <ul>
+            <li><a href="../">Home Page</a></li>
+            <li><a href="blog">My Blog</a></li>
+            <li><a href="about">About Me</a></li>
+            <li><a href="contactus">Contact Me</a></li>
+            <?
+            if (isset($_SESSION['username'])) {
+                echo "<li><a href='createarticle'>Create Article</a></li>";
+                echo "<li><a href='logout'>Logout</a></li>";
+            } else {
+                echo "<li><a href='login'>Login</a></li>";
+            }
+            ?>
+        </ul>
+    </nav>
+</header>
 <main class="grid-75 mobile-grid-100">
-    <h2>Jordan & Joe</h2>
+    <h2>Jordan & Joe yo</h2>
 
     <div id="section1" class="grid-33 tablet-grid-50 mobile-grid-100">
-        <p>Progress</p>
+        <p>This is my first section</p>
     </div>
 
     <div id="section2" class="grid-33 tablet-grid-50 mobile-grid-100">

2b4dd17b8cbead4a14619b71250a573ca98e7e96
Jordan <jwyoungman96@hotmail.co.uk>
2016-11-09 21:11:28 +0000

    Jordan

---
+++ GraniteCityGeeks.github.io/ClubsWebsite/backup.php
@@ -0,0 +1,31 @@
+<?php
+include("Header&Footer/header.php");
+?>
+<main class="grid-75 mobile-grid-100">
+    <h2>Jordan & Joe yo</h2>
+
+    <div id="section1" class="grid-33 tablet-grid-50 mobile-grid-100">
+        <p>This is my first section</p>
+    </div>
+
+    <div id="section2" class="grid-33 tablet-grid-50 mobile-grid-100">
+        <p>This is my second section</p>
+    </div>
+
+    <div id="section3" class="grid-33 tablet-grid-50 mobile-grid-100">
+        <p>This is my third section</p>
+    </div>
+</main>
+
+<aside class="grid-25 mobile-grid-100">
+    <h2>This is my right sidebar</h2>
+</aside>
+
+<div id="bottom" class="grid-100">
+    <h2>This is my bottom Content</h2>
+</div>
+
+<?php
+include("Header&Footer/footer.php");
+?>
+</html>

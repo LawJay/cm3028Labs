b3cbb4500cea130be754da80364e7951416f50c1
Jordan <jwyoungman96@hotmail.co.uk>
2016-11-09 19:04:32 +0000

    Jordan

--- GraniteCityGeeks.github.io/ClubsWebsite/Clubs.php
+++ GraniteCityGeeks.github.io/ClubsWebsite/Clubs.php
@@ -5,7 +5,25 @@
     <link rel="stylesheet" href="CoreCSS/unsemantic-grid-responsive-tablet.css">
 </head>
 <body>
-
+<header class="grid-80 prefix-10 suffix-10">
+    <h1>The Blog of Charles Babbage</h1>
+    <nav>
+        <ul>
+            <li><a href="../">Home Page</a></li>
+            <li><a href="blog">My Blog</a></li>
+            <li><a href="about">About Me</a></li>
+            <li><a href="contactus">Contact Me</a></li>
+            <?
+            if (isset($_SESSION['username'])) {
+                echo "<li><a href='createarticle'>Create Article</a></li>";
+                echo "<li><a href='logout'>Logout</a></li>";
+            } else {
+                echo "<li><a href='login'>Login</a></li>";
+            }
+            ?>
+        </ul>
+    </nav>
+</header>
 <main class="grid-75 mobile-grid-100">
     <h2>Jordan & Joe yo</h2>
 
